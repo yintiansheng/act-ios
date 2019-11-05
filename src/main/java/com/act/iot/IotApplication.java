d3e7c3cdfce56250b75d5bcf59fbc96fea5392e2
yintiansheng <577540265@qq.com>
2019-11-05 11:09:29 +0800

    提交一次

--- src/main/java/com/act/iot/IotApplication.java
+++ src/main/java/com/act/iot/IotApplication.java
@@ -1,13 +1,10 @@
 package com.act.iot;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class IotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IotApplication.class, args);
+		System.out.println("sssssssssssssssssssss");
 	}
-
 }
