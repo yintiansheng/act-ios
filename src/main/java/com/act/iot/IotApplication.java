3cda6d35dd71a9bec3cdddc6a63ffb03d8fc5ac2
yintiansheng <577540265@qq.com>
2019-11-05 09:08:38 +0800

    创建

---
+++ src/main/java/com/act/iot/IotApplication.java
@@ -0,0 +1,17 @@
+package com.act.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotApplication.class, args);
+
+
+
+
+	}
+
+}
